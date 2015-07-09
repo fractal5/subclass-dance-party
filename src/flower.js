8209e7db4ce0e8c20c0d5a53460642475935693e
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-07-09 09:46:08 -0700

    added assets

--- src/flower.js
+++ src/flower.js
@@ -9,7 +9,7 @@ MakeFlower.prototype.constructor = MakeFlower;
 
 MakePlant.prototype.selectSpecies = function() {
   var basePath = './assets/';
-  var species = ['flower-red.png', 'flower-blue.png'];
+  var species = ['flower-red.png', 'flower-blue.png', 'flower-yellow.png', 'flower-pink.png'];
   var selectedSpecies = species[Math.floor(Math.random() * species.length)].toString();
   var flowerImg = '<img class="flower-img" src="' + basePath + selectedSpecies + '">';
   var stemmImg = '<img class="stemm-img" src="' + basePath + 'flower-stemm.png"></div>';
